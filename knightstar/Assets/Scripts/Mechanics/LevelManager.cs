c125d05461d6ededc2ad4645bb28c6447e74d4d9
jen234 <70819650+jen234@users.noreply.github.com>
2022-06-12 15:19:49 +0100

    fixed scene bug
    i manged to fix one of the screen bugs that i was having but the virtual desktop bugged out

--- knightstar/Assets/Scripts/Mechanics/LevelManager.cs
+++ knightstar/Assets/Scripts/Mechanics/LevelManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour
+public class LevelManager : MonoBehaviour
 {
    public void QuitGame()
     {
